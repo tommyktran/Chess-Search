e06660f5b52081e080d0006c917cca367e9aef84
ivs-llc <yung.nguyen@ivsllc.com>
2021-11-18 22:09:30 -0500

    Reorganise code

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -13,3 +13,33 @@
 //};
 
 //getGame();
+
+var board1 = Chessboard('board1', 'start')
+let results;
+const chess = new Chess()
+
+function getGame(play = "", ratings = '1600,1800,2000,2200,2500', speeds = 'blitz,rapid,classical') {
+    fetch(`https://explorer.lichess.ovh/lichess?variant=standard&speeds=${speeds}&play=${play}&ratings=${ratings}`)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            //return data;
+            results = data;
+            document.getElementById('results').value = JSON.stringify(data);
+            document.getElementById('opening').innerText = results.opening.name;
+            chess.reset()
+            for (let move of document.getElementById("moves").value.split(",")) {
+                chess.move(move, { sloppy: true });
+                console.log(move);
+
+            }
+            board1.position(chess.fen());
+        })
+        .catch((error) => {
+            document.getElementById('results').value = 'Error ' + error.toString() + '\n\n Check if your input was correct.';
+            chess.reset();
+            board1.position(chess.fen());
+            document.getElementById('opening').innerText = '(None)';
+
+        });
+    };
